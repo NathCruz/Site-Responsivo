f6a119f7f5d33b3b0a5d0c4e751695c444276a1c
NathCruz <70970348+NathCruz@users.noreply.github.com>
2021-01-19 16:26:19 -0300

    servicos

--- js/main.js
+++ js/main.js
@@ -8,4 +8,22 @@ jQuery(document).ready(function ($) {
             $('#header').removeClass("active");
         }
     }
+
+    //ISOTOPE
+    let btns = $("#servicos .button-group button");
+    btns.click(function(e) {
+        $("#servicos .button-group button").removeClass("active");
+        e.target.classList.add("active");
+
+        let selector = $(e.target).attr("data-filter");
+        $("#servicos .grid").isotope({
+            filter: selector,   
+        });
+    });
+
+    $(window).on("load", function () {
+        $("#servicos .grid").isotope({
+            filter: "*",
+        });
+    });
 });
