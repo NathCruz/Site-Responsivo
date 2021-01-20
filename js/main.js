b167a7b729dff285dca5327d9e606c6dee70c83a
NathCruz <70970348+NathCruz@users.noreply.github.com>
2021-01-20 18:16:52 -0300

    footer

--- js/main.js
+++ js/main.js
@@ -37,4 +37,26 @@ jQuery(document).ready(function ($) {
             tCounter: "%curr% de %total%",
         },
     });
+
+    //OWL
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        dots: true,
+        lazyLoad: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 2,
+            },
+        },
+    });
 });
