d6ecd2435a06c6e0f7bbca83d54319583f3d1a8e
NathCruz <70970348+NathCruz@users.noreply.github.com>
2021-01-20 16:44:01 -0300

    final-servicos-recentes

--- js/main.js
+++ js/main.js
@@ -26,4 +26,15 @@ jQuery(document).ready(function ($) {
             filter: "*",
         });
     });
+
+    //MAGNIFY
+    $(" .grid .popup-link").magnificPopup({
+        type: "image",
+        gallery: {
+            enabled: true,
+            tPrev: "Anterior",
+            tNext: "Próxima",
+            tCounter: "%curr% de %total%",
+        },
+    });
 });
